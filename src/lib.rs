6fd43f482d3b0000c2c3a127696e9701a62c9ddd
Matthew Klein <matthew.klein@birchwood.ca>
2022-05-20 12:58:15 -0500

    Print output with resulting query
    - Make the `jq` bin configurable with "JQ_BIN"
    - The query will be printed to stdout, and passed to `jq` so it can be
      piped to another command.

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,59 @@
 use clap::Parser;
 use std::path::{Path, PathBuf};
-use std::process::{Command, ExitStatus, Stdio};
+use std::process::{Command, Stdio};
 
 #[derive(clap::Parser)]
-struct Opt {
+pub struct Opt {
+    /// Executable to call
+    #[clap(env, default_value = "jq")]
+    jq_bin: String,
+
     /// Name of the JSON file to read from (defaults to standard input)
     filename: Option<PathBuf>,
 
     /// Additional args passed to `jq`
     #[clap(last = true)]
-    jq: Vec<String>,
+    args: Vec<String>,
 }
 
-pub fn run() -> Result<Option<ExitStatus>, Error> {
+pub fn run() -> Result<(), Error> {
     let opt = Opt::parse();
+    let (cmd, path, is_temp) = build_cmd(&opt)?;
+    let query = get_query(cmd)?;
 
-    let (path, is_temp) = match opt.filename {
-        Some(filename) if filename.exists() => (filename, false),
+    eprintln!("{:?}", query);
+
+    let output = build_output_cmd(&opt.jq_bin, &path, &opt.args, &query)?.output()?;
+
+    print!("{}", String::from_utf8(output.stdout)?);
+
+    if is_temp {
+        std::fs::remove_file(path)?;
+    }
+
+    Ok(())
+}
+
+pub fn build_output_cmd(
+    cmd: &str,
+    input: &Path,
+    args: &[String],
+    output: &str,
+) -> Result<Command, Error> {
+    let f = Command::new("cat")
+        .arg(input.display().to_string())
+        .stdout(Stdio::piped())
+        .spawn()?;
+
+    let mut jq = Command::new(cmd);
+
+    jq.args(args).arg(output).stdin(f.stdout.unwrap());
+    Ok(jq)
+}
+
+pub fn build_cmd(opt: &Opt) -> Result<(Command, PathBuf, bool), Error> {
+    let (path, is_temp) = match &opt.filename {
+        Some(filename) if filename.exists() => (filename.clone(), false),
         Some(_) | None => {
             let mut stdin = std::io::stdin();
             let (mut file, path) = tempfile::NamedTempFile::new()?.keep()?;
@@ -26,13 +63,17 @@ pub fn run() -> Result<Option<ExitStatus>, Error> {
     };
 
     let input = path.display();
+    let jq_bin = opt.jq_bin.as_str();
 
     let echo = Command::new("echo")
         .arg("")
         .stdout(Stdio::piped())
         .spawn()?;
 
-    let jq_prefix = format!("jq --color-output --raw-output {}", opt.jq.join(" "));
+    let jq_prefix = format!(
+        "{jq_bin} --color-output --raw-output {}",
+        opt.args.join(" ")
+    );
 
     let jq_history_file = Path::new(concat!(env!("HOME"), "/.jq_repl_history")).display();
 
@@ -43,8 +84,8 @@ pub fn run() -> Result<Option<ExitStatus>, Error> {
         ]
     };
 
-    let mut fzf = Command::new("fzf")
-        .args(["--print-query", "--preview-window=up,99%"])
+    let mut cmd = Command::new("fzf");
+    cmd.args(["--disabled", "--print-query", "--preview-window=up,99%"])
         .arg(format!("--history={jq_history_file}"))
         .arg(format!("--preview={jq_prefix} {{q}} {input}"))
         .arg(format!(
@@ -63,21 +104,30 @@ pub fn run() -> Result<Option<ExitStatus>, Error> {
         .args(bind("alt-s", "alt-S", "--slurp"))
         .args(bind("alt-c", "alt-C", "--compact-output"))
         .stdin(echo.stdout.unwrap())
-        .spawn()?;
+        .stdout(Stdio::piped());
 
-    let status = match fzf.try_wait() {
-        Ok(Some(status)) => Some(status),
-        Ok(None) => Some(fzf.wait()?),
-        Err(err) => {
-            return Err(Error::from(err));
+    Ok((cmd, path, is_temp))
+}
+
+pub fn get_query(mut fzf: Command) -> Result<String, Error> {
+    let child = fzf.spawn()?;
+
+    let query = match child.wait_with_output() {
+        Ok(output) if output.status.success() => {
+            let out = String::from_utf8(output.stdout)?;
+            let out = out.trim();
+            Ok(out.to_string())
         }
+        Ok(output) => Err(Error::Fzf(output.status)),
+        Err(err) => Err(err.into()),
+        // Ok(Some(status)) => Some(status),
+        // Ok(None) => Some(fzf.wait()?),
+        // Err(err) => {
+        //     return Err(Error::from(err));
+        // }
     };
 
-    if is_temp {
-        std::fs::remove_file(path)?;
-    }
-
-    Ok(status)
+    query
 }
 
 pub use error::Error;
@@ -88,6 +138,8 @@ mod error {
     pub enum Error {
         Io(std::io::Error),
         TmpPersist(tempfile::PersistError),
+        Utf8(std::string::FromUtf8Error),
+        Fzf(std::process::ExitStatus),
     }
 
     impl fmt::Display for Error {
@@ -95,6 +147,8 @@ mod error {
             match self {
                 Self::Io(err) => err.fmt(f),
                 Self::TmpPersist(err) => err.fmt(f),
+                Self::Utf8(err) => err.fmt(f),
+                Self::Fzf(err) => write!(f, "{err}"),
             }
         }
     }
@@ -110,6 +164,12 @@ mod error {
     impl From<tempfile::PersistError> for Error {
         fn from(err: tempfile::PersistError) -> Self {
             Self::TmpPersist(err)
+        }
+    }
+
+    impl From<std::string::FromUtf8Error> for Error {
+        fn from(err: std::string::FromUtf8Error) -> Self {
+            Self::Utf8(err)
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,10 @@
 use std::process::ExitCode;
 
 fn main() -> ExitCode {
-    match jq_repl::run() {
-        Ok(exit_status) => match exit_status {
-            Some(status) if status.success() => ExitCode::SUCCESS,
-            Some(status) => {
-                eprintln!("{status}");
-                ExitCode::FAILURE
-            }
-            None => ExitCode::SUCCESS,
-        },
-        Err(err) => {
-            eprintln!("{:?}", err);
-            ExitCode::FAILURE
-        }
+    if let Err(err) = jq_repl::run() {
+        eprintln!("{err}");
+        ExitCode::FAILURE
+    } else {
+        ExitCode::SUCCESS
     }
 }
