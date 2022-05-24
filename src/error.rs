bd57f859c31a170aba9e811ecd33d1614e878a69
Matthew Klein <matthew.klein@birchwood.ca>
2022-05-24 10:46:50 -0500

    Extract error module to new file

---
+++ src/error.rs
@@ -0,0 +1,40 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub enum Error {
+    Io(std::io::Error),
+    TmpPersist(tempfile::PersistError),
+    Utf8(std::string::FromUtf8Error),
+    Fzf(std::process::ExitStatus),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Io(err) => err.fmt(f),
+            Self::TmpPersist(err) => err.fmt(f),
+            Self::Utf8(err) => err.fmt(f),
+            Self::Fzf(err) => write!(f, "{err}"),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Self {
+        Self::Io(err)
+    }
+}
+
+impl From<tempfile::PersistError> for Error {
+    fn from(err: tempfile::PersistError) -> Self {
+        Self::TmpPersist(err)
+    }
+}
+
+impl From<std::string::FromUtf8Error> for Error {
+    fn from(err: std::string::FromUtf8Error) -> Self {
+        Self::Utf8(err)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
+mod error;
+
 use clap::Parser;
+pub use error::Error;
 use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
 
@@ -135,48 +138,4 @@ pub fn get_query(mut fzf: Command) -> Result<String, Error> {
     };
 
     query
-}
-
-pub use error::Error;
-mod error {
-    use std::fmt;
-
-    #[derive(Debug)]
-    pub enum Error {
-        Io(std::io::Error),
-        TmpPersist(tempfile::PersistError),
-        Utf8(std::string::FromUtf8Error),
-        Fzf(std::process::ExitStatus),
-    }
-
-    impl fmt::Display for Error {
-        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-            match self {
-                Self::Io(err) => err.fmt(f),
-                Self::TmpPersist(err) => err.fmt(f),
-                Self::Utf8(err) => err.fmt(f),
-                Self::Fzf(err) => write!(f, "{err}"),
-            }
-        }
-    }
-
-    impl std::error::Error for Error {}
-
-    impl From<std::io::Error> for Error {
-        fn from(err: std::io::Error) -> Self {
-            Self::Io(err)
-        }
-    }
-
-    impl From<tempfile::PersistError> for Error {
-        fn from(err: tempfile::PersistError) -> Self {
-            Self::TmpPersist(err)
-        }
-    }
-
-    impl From<std::string::FromUtf8Error> for Error {
-        fn from(err: std::string::FromUtf8Error) -> Self {
-            Self::Utf8(err)
-        }
-    }
 }
