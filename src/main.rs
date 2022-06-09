cfe0a8f0a9651419562318467e0f933339d04008
Matthew Klein <matthew.klein@birchwood.ca>
2022-06-09 13:42:37 -0500

    Exit with the same exit code fzf exited with, if it failed

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+use jq_repl::Error;
+
 fn main() {
     if let Err(err) = jq_repl::run() {
+        if let Error::Fzf(status) = err {
+            if let Some(code) = status.code() {
+                if !status.success() {
+                    std::process::exit(code);
+                }
+            }
+        }
         eprintln!("{err}");
         std::process::exit(1);
     }
