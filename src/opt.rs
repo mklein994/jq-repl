367ee4dc294e86a348284a4a14d3d0d1f91c79e3
Matthew Klein <mklein994@gmail.com>
2022-11-19 21:33:27 -0600

    Upgrade Opt struct to clap 4 syntax

--- src/opt.rs
+++ src/opt.rs
@@ -2,62 +2,62 @@ use std::path::PathBuf;
 
 #[allow(clippy::struct_excessive_bools)]
 #[derive(Debug, clap::Parser)]
-#[clap(version, about)]
+#[command(version, about)]
 pub struct Opt {
     /// Executable to call
-    #[clap(long, env = "JQ_BIN", default_value = "gojq")]
+    #[arg(long, env = "JQ_BIN", default_value = "gojq")]
     pub bin: String,
 
     /// Path to the history file (use ^P and ^N to navigate it)
     ///
     /// History is only recorded when query is accepted (enter is pressed).
-    #[clap(long, env = "JQ_REPL_HISTORY", default_value = concat!(env!("HOME"), "/.jq_repl_history"))]
+    #[arg(long, env = "JQ_REPL_HISTORY", default_value = concat!(env!("HOME"), "/.jq_repl_history"))]
     pub history_file: PathBuf,
 
     /// Usw `null` as input value
     ///
     /// This is the default when no file path was given and standard input is from an
     /// interactive terminal.
-    #[clap(short, long)]
+    #[arg(short, long)]
     pub null_input: bool,
 
     /// Print the fzf command that would be run to stdout and exit.
-    #[clap(long)]
+    #[arg(long)]
     pub show_fzf_command: bool,
 
     /// Disable the default arguments
-    #[clap(long = "no-default-args", action(clap::ArgAction::SetFalse))]
+    #[arg(long = "no-default-args", action(clap::ArgAction::SetFalse))]
     pub use_default_args: bool,
 
     /// The flag passed to jq inside fzf to show color
-    #[clap(long, allow_hyphen_values = true, default_value = "-C")]
+    #[arg(long, allow_hyphen_values = true, default_value = "-C")]
     pub color_flag: String,
 
     /// The flag passed to jq inside fzf to disable color
-    #[clap(long, allow_hyphen_values = true, default_value = "-M")]
+    #[arg(long, allow_hyphen_values = true, default_value = "-M")]
     pub no_color_flag: String,
 
     /// The flag passed to jq inside fzf to use a compact format
-    #[clap(long, allow_hyphen_values = true, default_value = "-c")]
+    #[arg(long, allow_hyphen_values = true, default_value = "-c")]
     pub compact_flag: String,
 
     /// Name of the JSON file to read from (defaults to standard input)
     pub filename: Option<PathBuf>,
 
     /// Pager to pipe output to
-    #[clap(long, default_value = "less")]
+    #[arg(long, default_value = "less")]
     pub pager: String,
 
     /// Options to pass to the pager
-    #[clap(long, allow_hyphen_values = true)]
+    #[arg(long, allow_hyphen_values = true)]
     pub pager_options: Vec<String>,
 
     /// Show versions of all relevant executables
-    #[clap(long)]
+    #[arg(long)]
     pub version_verbose: bool,
 
     /// Additional args passed to `jq`
-    #[clap(last = true)]
+    #[arg(last = true)]
     pub args: Vec<String>,
 }
 
