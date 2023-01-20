eafc69182a184b3f7c216cd39bd414d30412bf62
Matthew Klein <matthew.klein@birchwood.ca>
2023-01-19 18:02:13 -0600

    Make integration test not specific to one computer

--- tests/integration.rs
+++ tests/integration.rs
@@ -3,6 +3,8 @@ use std::process::Command;
 #[test]
 fn check_fzf_command_output() {
     let output = Command::new(env!("CARGO_BIN_EXE_jq-repl"))
+        .arg("--history-file")
+        .arg("/tmp/jq_repl_history")
         .arg("--show-fzf-command")
         .arg("/tmp/foo.json")
         .output()
@@ -15,7 +17,7 @@ fn check_fzf_command_output() {
 --print-query \
 $'--preview-window=up,99%,border-bottom' \
 $'--info=hidden' \
-$'--history=/home/matthew/.jq_repl_history' \
+$'--history=/tmp/jq_repl_history' \
 $'--preview=gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C {q} /tmp/foo.json' \
 $'--bind=ctrl-k:kill-line,pgup:preview-page-up,pgdn:preview-page-down,alt-w:toggle-preview-wrap,home:preview-top,end:preview-bottom' \
 $'--bind=alt-s:change-preview:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C --slurp {q} /tmp/foo.json' \
