043b9c1728816df4f387fc6f08395343f502a886
Matthew Klein <mklein994@gmail.com>
2023-01-28 17:03:12 -0600

    Don't pass any input to the integration test
    This makes it simpler to check that it's roughly doing the right thing,
    since otherwise it would have to read the files first, to determine if
    any of them are the result of shell substitution, and can only be read
    from once.

--- tests/fzf_arguments.rs
+++ tests/fzf_arguments.rs
@@ -1,4 +1,4 @@
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 #[test]
 fn check_fzf_command_output() {
@@ -6,7 +6,8 @@ fn check_fzf_command_output() {
         .arg("--history-file")
         .arg("/tmp/jq_repl_history")
         .arg("--show-fzf-command")
-        .arg("/tmp/foo.json")
+        .arg("-n")
+        .stdin(Stdio::inherit())
         .output()
         .unwrap();
 
@@ -22,19 +23,19 @@ $'--info=hidden' \
 --header-first \
 $'--header=M-e: editor \xE2\x81\x84 M-v: vd \xE2\x81\x84 M-l: pager \xE2\x81\x84 ^<space>: gron' \
 $'--history=/tmp/jq_repl_history' \
-$'--preview=gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C {q} /tmp/foo.json' \
+$'--preview=gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n {q} ' \
 $'--bind=ctrl-k:kill-line,pgup:preview-page-up,pgdn:preview-page-down,alt-w:toggle-preview-wrap,home:preview-top,end:preview-bottom' \
-$'--bind=alt-s:change-prompt(-s> )+change-preview:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C --slurp {q} /tmp/foo.json' \
-$'--bind=alt-S:change-prompt(> )+change-preview:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C {q} /tmp/foo.json' \
-$'--bind=alt-c:change-prompt(-c> )+preview:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -c {q} /tmp/foo.json' \
-$'--bind=alt-C:change-prompt(> )+preview:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C {q} /tmp/foo.json' \
-$'--bind=ctrl-space:change-prompt(gron> )+change-preview:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -M {q} /tmp/foo.json | gron --colorize' \
-$'--bind=alt-space:change-prompt(> )+change-preview:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C {q} /tmp/foo.json' \
-$'--bind=alt-e:execute:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -M {q} /tmp/foo.json | nvim -c \'set ft=json\' -' \
-$'--bind=alt-v:execute:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -M {q} /tmp/foo.json | vd --filetype json' \
-$'--bind=alt-V:execute:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -M {q} /tmp/foo.json | vd --filetype csv' \
-$'--bind=alt-l:execute:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C {q} /tmp/foo.json | less -R' \
-$'--bind=alt-L:execute:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -M {q} /tmp/foo.json | bat --language json' < /dev/null
+$'--bind=alt-s:change-prompt(-s> )+change-preview:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n --slurp {q} ' \
+$'--bind=alt-S:change-prompt(> )+change-preview:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n {q} ' \
+$'--bind=alt-c:change-prompt(-c> )+preview:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n -c {q} ' \
+$'--bind=alt-C:change-prompt(> )+preview:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n {q} ' \
+$'--bind=ctrl-space:change-prompt(gron> )+change-preview:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n -M {q}  | gron --colorize' \
+$'--bind=alt-space:change-prompt(> )+change-preview:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n {q} ' \
+$'--bind=alt-e:execute:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n -M {q}  | nvim -c \'set ft=json\' -' \
+$'--bind=alt-v:execute:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n -M {q}  | vd --filetype json' \
+$'--bind=alt-V:execute:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n -M {q}  | vd --filetype csv' \
+$'--bind=alt-l:execute:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n {q}  | less -R' \
+$'--bind=alt-L:execute:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n -M {q}  | bat --language json' < /dev/null
 "##;
 
     assert_eq!(
