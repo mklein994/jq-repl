883693ab92f52df4fdaa0233ce503ed5cf438ed5
Matthew Klein <matthew.klein@birchwood.ca>
2023-11-17 16:45:13 -0600

    Simplify hashes and add bat paging flag to test output

--- tests/fzf_arguments.rs
+++ tests/fzf_arguments.rs
@@ -16,7 +16,7 @@ fn check_fzf_command_output() {
     let stdout = String::from_utf8(output.stdout).unwrap();
 
     let line = line!();
-    let expected = r##"#!/bin/bash
+    let expected = r#"#!/bin/bash
 
 fzf \
 --disabled \
@@ -41,8 +41,8 @@ $'--bind=alt-j:execute:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n -M {q}  | v
 $'--bind=alt-J:execute:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n -c -M {q}  | vd --filetype jsonl' \
 $'--bind=alt-v:execute:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n -M {q}  | vd --filetype csv' \
 $'--bind=alt-l:execute:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n -M {q}  | less ' \
-$'--bind=alt-L:execute:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n -M {q}  | bat --language json' < /dev/null
-"##;
+$'--bind=alt-L:execute:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n -M {q}  | bat --language json --paging always' < /dev/null
+"#;
 
     assert_eq!(
         expected.lines().count(),
