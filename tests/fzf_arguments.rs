405ce522d34128b2f7d89530407c2078e2a43e17
Matthew Klein <matthew.klein@birchwood.ca>
2023-11-17 16:49:47 -0600

    Turn fzf_arguments test into a snapshot test

--- tests/fzf_arguments.rs
+++ tests/fzf_arguments.rs
@@ -15,54 +15,5 @@ fn check_fzf_command_output() {
 
     let stdout = String::from_utf8(output.stdout).unwrap();
 
-    let line = line!();
-    let expected = r#"#!/bin/bash
-
-fzf \
---disabled \
-$'--preview-window=up,99%,border-bottom' \
-$'--info=hidden' \
---header-first \
-$'--prompt=-n> ' \
-$'--header=M-e: editor \xE2\x81\x84 M-j: vd \xE2\x81\x84 M-l: pager \xE2\x81\x84 M-g: braille \xE2\x81\x84 ^<space>: gron' \
-$'--history=/tmp/jq_repl_history' \
-$'--preview-label-pos=-1' \
-$'--bind=change:transform-preview-label:printf "%s" {q} | charcounter ' \
-$'--preview=gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n {q} ' \
-$'--bind=ctrl-k:kill-line,pgup:preview-page-up,pgdn:preview-page-down,alt-w:toggle-preview-wrap,home:preview-top,end:preview-bottom' \
-$'--bind=alt-s:change-prompt(-ns> )+change-preview:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n --slurp {q} ' \
-$'--bind=alt-S:change-prompt(-n> )+change-preview:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n {q} ' \
-$'--bind=alt-c:change-prompt(-nc> )+change-preview:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n -c {q} ' \
-$'--bind=alt-C:change-prompt(-n> )+change-preview:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n {q} ' \
-$'--bind=ctrl-space:change-prompt(-n gron> )+change-preview:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n -M {q}  | gron --colorize' \
-$'--bind=alt-space:change-prompt(-n> )+change-preview:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n {q} ' \
-$'--bind=alt-g:change-prompt(-n braille> )+change-preview:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n -M {q}  | braille --modeline' \
-$'--bind=alt-G:change-prompt(-n> )+change-preview:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n {q} ' \
-$'--bind=alt-e:execute:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n -M {q}  | nvim -c \'set ft=json\' -' \
-$'--bind=alt-j:execute:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n -M {q}  | vd --filetype json' \
-$'--bind=alt-J:execute:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n -c -M {q}  | vd --filetype jsonl' \
-$'--bind=alt-v:execute:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n -M {q}  | vd --filetype csv' \
-$'--bind=alt-l:execute:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n -M {q}  | less ' \
-$'--bind=alt-L:execute:gojq -L ~/.jq -L ~/.jq/.jq --raw-output -C -n -M {q}  | bat --language json --paging always' < /dev/null
-"#;
-
-    assert_eq!(
-        expected.lines().count(),
-        stdout.lines().count(),
-        "expected != actual line count.\nstdout:\n{stdout}\n\nstderr:\n{}",
-        String::from_utf8_lossy(&output.stderr)
-    );
-
-    if expected != stdout {
-        for (line_number, (expected_line, actual_line)) in
-            expected.lines().zip(stdout.lines()).enumerate()
-        {
-            assert_eq!(
-                expected_line,
-                actual_line,
-                "failed on line {}",
-                line as usize + line_number + 1
-            );
-        }
-    }
+    insta::assert_snapshot!(stdout);
 }
