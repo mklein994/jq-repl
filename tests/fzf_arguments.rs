d2e3b8c35384862076c22e3ee02331db0665a666
Matthew Klein <mklein994@gmail.com>
2026-03-15 16:35:43 -0500

    Don't inherit JQ_REPL_* variables in tests

--- tests/fzf_arguments.rs
+++ tests/fzf_arguments.rs
@@ -4,6 +4,16 @@ use std::process::{Command, Stdio};
 fn check_fzf_command_output() {
     let output = Command::new(env!(concat!("CARGO_BIN_EXE_", clap::crate_name!())))
         .env("JQ_REPL_TEST", "true")
+        // Internal programs
+        .env_remove("JQ_REPL_TRANSFORM_BIN")
+        .env_remove("JQ_REPL_CHARCOUNTER_BIN")
+        .env_remove("JQ_REPL_COMPLETION_BIN")
+        // Settings
+        .env_remove("JQ_REPL_HISTORY")
+        .env_remove("JQ_REPL_LIB")
+        // Default programs
+        .env_remove("JQ_BIN")
+        .env_remove("JQ_REPL_JQ_BIN")
         .arg("--history-file")
         .arg("/tmp/jq_repl_history")
         .arg("--config")
