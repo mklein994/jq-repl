4da6a63e8b762a5809d2a49c06e0201288dc61a4
Matthew Klein <mklein994@gmail.com>
2026-03-22 14:54:34 -0500

    Add some whitespace between Error variants

--- src/error.rs
+++ src/error.rs
@@ -4,14 +4,19 @@ use thiserror::Error as ThisError;
 pub enum Error {
     #[error(transparent)]
     Io(#[from] std::io::Error),
+
     #[error(transparent)]
     TmpPersist(#[from] tempfile::PersistError),
+
     #[error(transparent)]
     Utf8(#[from] std::string::FromUtf8Error),
+
     #[error("{0}")]
     Fzf(std::process::ExitStatus),
+
     #[error(transparent)]
     Clap(#[from] clap::Error),
+
     #[error(transparent)]
     Config(#[from] crate::config::ConfigError),
 }
